a0aaa04bbca1eda1591f47d0b1923633e3a4d336
nuno <ruiduidahh@Gmail.com>
2023-08-30 16:15:20 +0200

    nathan cooking

--- sidescroller/Assets/noteDetector.cs
+++ sidescroller/Assets/noteDetector.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class noteDetector : MonoBehaviour
 {
+
+    public LayerMask noPlayer;
+    public TextMeshProUGUI scoreUI;
+    public int Scorecount;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Collider2D collider = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), noPlayer);
+        if (collider != null)
+        {
+            if (Input.GetKeyDown("space") && collider.gameObject.name != "triggerzone")
+            {
+                Scorecount++;
+
+                scoreUI.text = Scorecount.ToString();
+                Destroy(collider.gameObject);
+            }
+            else
+            {
+
+
+            }
+            Debug.Log("up key was pressed");
+
+        }
+        Debug.Log(collider.gameObject.name);
     }
 }
